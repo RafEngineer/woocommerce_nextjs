bb482761143d8846d773824e9cd63095c54ad0dc
RafEngineer <rafaelandradeferreira@hotmail.com>
2021-02-27 10:28:54 -0300

    [feat](main):[Add Next]

--- raf_enextjs/config/wooConfig.js
+++ raf_enextjs/config/wooConfig.js
@@ -1,7 +1,7 @@
 const wooConfig = {
     siteUrl: 'https://site.com',
-    consumerKey: 'ck_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-    consumerSecret: 'cs_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    consumerKey: 'ck_e100a6cc8e5a931d2b5a4dcc0e9eef2126bec3c8',
+    consumerSecret: 'cs_6f2583a5a1013b2de0b5053d4b5fafe9c5d89019',
 }
 
 module.exports = wooConfig;
